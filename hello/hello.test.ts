e421194947bc774f7fd14f52e28e464c942d5647
Tenor <tenor@fmail.co.uk>
2025-05-20 10:25:30 -0400

    add code for als bug

--- hello/hello.test.ts
+++ hello/hello.test.ts
@@ -1,5 +1,17 @@
-import { describe, expect, test } from "vitest";
+import { beforeAll, describe, expect, test } from "vitest";
 import { get } from "./hello";
+import { AsyncLocalStorage } from "node:async_hooks";
+
+const als = new AsyncLocalStorage<{ foo: string }>()
+
+beforeAll(() => {
+  als.enterWith({ foo: 'bar' })
+  const store = als.getStore()
+
+  if (store == null) {
+    throw new Error('missing store')
+  }
+})
 
 describe("get", () => {
   test("should combine string with parameter value", async () => {
